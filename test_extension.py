cf78924e0634813c6d97ce6d1825d826f85fe76c
K Kollmann <git@k.kollmann.moe>
2016-07-19 20:02:05 +0200

    script to test the methods created with C

---
+++ test_extension.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Look up Pokémon by their index number.
+
+Companion Python code to extend.c, which contains the underlying C code.
+First try at extending Python with C.
+
+Copyright (c) 2016 K Kollmann <code∆k.kollmann·moe>
+"""
+
+import pokemon_lookup as pi
+
+# prompt for a number
+print("Find a Pokémon based on its index number?")
+print("Input YES or NO:")
+choice = input()
+
+if choice:
+    if choice.lower().startswith('y', 0, 1):
+        index = input("Enter a number to look up: ")
+        if index:
+            poke = pi.lookup_no(int(index))
+            print("Pokémon #{} is {}.".format(index, poke))
+        else:
+            print("Not a valid number.")
+    elif choice.lower().startswith('n', 0, 1):
+        pi.no_search()
+    else:
+        print("That... was not a valid choice.")
+else:
+    print("No valid input, quitting program.")
